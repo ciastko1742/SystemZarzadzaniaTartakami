c4008bc540382c05a84252e5fcc33e655e57d1ca
msybilski <m.sybilski@wieza.pl>
2021-01-19 18:44:47 +0100

    Dodanie tabeli Transport

---
+++ tartak/src/Entity/Transport.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TransportRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TransportRepository::class)
+ */
+class Transport
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
