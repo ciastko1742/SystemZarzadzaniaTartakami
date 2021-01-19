2273e1b0b0cad5c2e5d8e2a77a3c9c1e870d21bb
msybilski <m.sybilski@wieza.pl>
2021-01-19 18:46:16 +0100

    Dodanie tabeli Order

---
+++ tartak/src/Entity/Order.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=OrderRepository::class)
+ * @ORM\Table(name="`order`")
+ */
+class Order
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $id_cart;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
