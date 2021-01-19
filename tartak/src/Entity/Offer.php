75f8f3f63bb9dc9fbf183cbd43667b16853adb56
msybilski <m.sybilski@wieza.pl>
2021-01-19 18:28:07 +0100

    Dodanie tabeli Groups

---
+++ tartak/src/Entity/Offer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OfferRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=OfferRepository::class)
+ */
+class Offer
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $id_calculation;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $id_user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
