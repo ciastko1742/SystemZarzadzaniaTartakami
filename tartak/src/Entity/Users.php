a1638187e17d2f5990b9180c51ac03f864520e25
msybilski <m.sybilski@wieza.pl>
2021-01-27 22:52:17 +0100

    zmiany w klassach

--- tartak/src/Entity/Users.php
+++ tartak/src/Entity/Users.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Repository\UsersRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
