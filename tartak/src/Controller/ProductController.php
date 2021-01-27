6eaaac68bec7224f8888c7b1de08d9bf8339aea4
ciastko1742 <mateusz.wrzaszczak1997@gmail.com>
2021-01-27 22:54:55 +0100

    dodanie listy produktow

--- tartak/src/Controller/ProductController.php
+++ tartak/src/Controller/ProductController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use App\Repository\MaterialRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,8 +15,11 @@ class ProductController extends AbstractController
     /**
      * @Route("/welcome", name="welcome")
      */
-    public function index():Response{
-        return $this->render('product/index.html.twig');
+    public function index(MaterialRepository $materialRepository):Response{
+        return $this->render('product/index.html.twig',
+        [
+            'products'=>$materialRepository->findAll()
+        ]);
     }
 
 
--- tartak/src/Entity/Users.php
+++ tartak/src/Entity/Users.php
@@ -3,8 +3,10 @@
 namespace App\Entity;
 
 use App\Repository\UsersRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints\Collection;
 
 /**
  * @ORM\Entity(repositoryClass=UsersRepository::class)
